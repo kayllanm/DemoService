6c2e5b1195f0fbc48fdff082b3a95ecbc766b648
Kayllanm <Kayllanm@gmail.com>
2016-05-23 12:09:18 +0200

    Spring Rest Docs implemented

--- src/test/java/hello/PersonTest.java
+++ src/test/java/hello/PersonTest.java
@@ -1,10 +1,56 @@
 package hello;
 
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentation;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.restdocs.hypermedia.HypermediaDocumentation;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.restdocs.payload.PayloadDocumentation;
 
+
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class) 
 public class PersonTest {
+
+	@Rule
+    public RestDocumentation restDocumentation = new RestDocumentation("target/generated-snippets");
+
+	@Autowired
+	private WebApplicationContext context;
+	private MockMvc mockMvc;
+
+	// @Before
+	// public void setUp() {
+	//     this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+	//             .apply(documentationConfiguration(this.restDocumentation))
+	//             .build(); 
+	// }
+	
+	// @Test
+	// public void invokeRoot() throws Exception {
+	// 	this.mockMvc.perform(get("/").accept(MediaType.APPLICATION_JSON)) 
+	//     .andExpect(status().isOk()) 
+	//     .andDo(document("index"));
+	// }
 
 	@Test
 	public void test_create_person_instance() {
